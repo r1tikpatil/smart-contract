9d2bd44ecf997df07dfa85d2334f61defab05fe7
r1tikpatil <ritikpatil566@gmail.com>
2023-10-25 18:11:33 +0530

    deployed a contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,27 +2,21 @@ const assert = require("assert");
 const ganache = require("ganache");
 const { Web3 } = require("web3");
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
 
-class Car {
-  park() {
-    return "stopped";
-  }
+let accounts;
+let inbox;
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
 
-  drive() {
-    return "vroom";
-  }
-}
-
-let car;
-beforeEach(() => {
-  car = new Car();
+  inbox = await new web3.eth
+    .Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
-describe("Testing->", () => {
-  it("can park", () => {
-    assert.equal(car.park(), "stopped");
-  });
-  it("can drive", () => {
-    assert.equal(car.drive(), "vroom");
+describe("Inbox", () => {
+  it("deploy a contract", () => {
+    console.log(inbox);
   });
 });
