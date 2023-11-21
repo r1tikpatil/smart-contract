8b23881acf4bf83aa7d58f1b52e525cf5770dce9
r1tikpatil <ritikpatil566@gmail.com>
2023-11-21 15:55:14 +0530

    deploying using goerli

--- deploy.js
+++ deploy.js
@@ -1,5 +1,23 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const { interface, bytecode } = require("./compile");
 
-// deploy code will go here
+const provider = new HDWalletProvider(
+  "erupt xxx replace aspect xxxx bitter obscure rug idea xxxxxxx spy daring",
+  "https://goerli.infura.io/v3/0484be01d7d54exx829c4aecab6xxxxx"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
